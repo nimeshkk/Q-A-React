445cf8ad9e0949b9949927cf58e39da4d5485ea5
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-01 01:03:30 +0530

    animation

--- src/components/Q.js
+++ src/components/Q.js
@@ -1,15 +1,18 @@
 
-
+import React from 'react'
+import "./Q.css"
 
 export default function Q1() {
   return (
     <div>
-      <div className="question">what....</div>
-       <div className="answers">
+       <div className="Q1">
+         <div className="question">what....</div>
+         <div className="answers ">
             <div className="answer">answer 1</div>
-            <div className="answer">answer 2</div>
+            <div className="answer correct ">answer 2</div>
             <div className="answer">answer 3</div>
             <div className="answer">answer 4</div>
+        </div>
        </div>
     </div>
   )
