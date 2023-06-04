2827cbad18627389268f8cd123bbc295972f54a2
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-05 00:13:04 +0530

    new

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,26 +2,39 @@ import React, { useRef, useEffect, useState } from "react";
 
 export default function Countdown() {
   const [num, setNum] = useState(5);
-  
- 
-  
+  const [earned, setEarned] = useState(false);
+
   let intervalRef = useRef();
-  
-  const decreaseNum = () => setNum((prev) => prev - 1);
+
+  const decreaseNum = () => {
+    setNum((prev) => prev - 1);
+  };
 
   useEffect(() => {
-    
     intervalRef.current = setInterval(decreaseNum, 1000);
 
     return () => clearInterval(intervalRef.current);
   }, []);
-  
+
+  useEffect(() => {
+    if (num === 0) {
+      setEarned(true);
+    }
+  }, [num]);
+
   return (
     <div>
-      <div>{num}</div>
-      
+      {earned ? (
+        <div>Earned!</div>
+      ) : (
+        <div>
+          <div>{num}</div>
+        </div>
+      )}
     </div>
   );
 }
+
+
 
 
